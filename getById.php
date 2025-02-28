29502aa33f84f2642647096631fee09ae33e5b31
Kansokmean9 <kansokmean006@gmail.com>
2025-03-01 01:49:27 +0700

    mean

--- getById.php
+++ getById.php
@@ -3,6 +3,6 @@ require_once 'config/database.php';
 require_once 'library/library.php';
 $tbname = 'users';
 $criteria = "id";
-$values = 2;
+$values = 1;
 $labseven = new labSeven();
 $labseven ->getById($tbname, $criteria, $values);
--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 require_once 'config/database.php';
 require_once 'library/library.php';
     $tbname = 'users';
-    $criteria = "id" ;
-    $values = 3;
     $labseven = new labSeven();
-    
+
     $labseven ->getAll($tbname);
