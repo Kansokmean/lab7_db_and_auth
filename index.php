8d23cad069c02a826452b540a9d0180ca35c37d5
Kan Sokmean <156630441+Kansokmean@users.noreply.github.com>
2025-03-12 22:52:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php
 require_once 'config/database.php';
 require_once 'library/library.php';
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
-    $tbname = 'students';
-    $labeight = new labEight();
-    $data = $labeight ->getAll($tbname);
+    $tbname = 'users';
+    $labseven = new labSeven();
 
-    echo json_encode([
-        'result'=> true,
-        'message'=> 'Get students successfully',
-        'data'=> $data
-    ]);
+    $labseven ->getAll($tbname);
